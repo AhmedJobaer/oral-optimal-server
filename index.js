a1d336af68eca2108a14f21c21cc06bebb1497bd
jobaer-ahmed <ajobaer08@gmail.com>
2022-11-08 22:31:13 +0800

    mongodb added

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('oralOptimal').collection('services');
+
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+
+        /*  const user = {
+             name: 'test',
+             email: 'test@gmail.com',
+             title: 'Asif Ahmed'
+         }
+         const result = await serviceCollection.insertOne(user);
+         console.log(result); */
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send('Oral Optimal server is running!')
+})
+
+app.listen(port, () => {
+    console.log(`Oral Optimal server running on ${port}`)
+})
